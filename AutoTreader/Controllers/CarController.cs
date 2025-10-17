c135a82dbff67e90c0d5b9a660aeb7919c495d7d
Ballam906 <90650752+Ballam906@users.noreply.github.com>
2025-10-17 11:48:07 +0200

    ok

---
+++ AutoTreader/Controllers/CarController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using AutoTreader.Models;
+
+namespace AutoTreader.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CarController : Controller
+    {
+        [HttpGet]
+        public ActionResult<Car> GetAllRecord()
+        {
+            using (var context = new CarDBContest())
+            {
+                var car = context.Cars.ToList();
+                if (car != null)
+                {
+                    return Ok(car);
+                }
+                return BadRequest(new {message = "Sikertelen lekérdezés"});
+            }
+        }
+    }
+}
---
+++ AutoTreader/Models/CarDBContest.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AutoTreader.Models
+{
+    public class CarDBContest : DbContext
+    {
+        public CarDBContest()
+        {
+
+        }
+
+        public CarDBContest(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Car> Cars { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySQL("server=localhost;database=autotrader;user=root;password=;SslMode=None");
+        }
+    }
+}
